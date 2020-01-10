33098cdd22a8886404d879e5ce3c1ed45c16a21a
JhosuaSinambela <Jhosuagaruda389@gmail.com>
2020-01-10 22:14:40 +0700

    Set DB

--- koneksi.php
+++ koneksi.php
@@ -1,9 +1,9 @@
 
-<?php 
-$koneksi = mysqli_connect("localhost","id12193397_kopilintong","sukses123","id12193397_kopilintong");
+<?php
+$koneksi = mysqli_connect("us-cdbr-iron-east-05.cleardb.net","b7c36e2019969a","c7e4165b","heroku_7365398e2868881");
 
 if (mysqli_connect_errno()){
 	echo "Koneksi database gagal : " . mysqli_connect_error();
 }
- 
+
 ?>
